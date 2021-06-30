3bae60d247c7bd45e86e2aa92298f00ff511e8ca
Casey R <crowlandsdev@gmail.com>
2021-06-30 10:11:48 -0400

    Finished question list

--- assets/js/questions.js
+++ assets/js/questions.js
@@ -6,47 +6,47 @@ let questions = [
     },
     {
         title: "What is the name of the main character in the game?",
-        choice:["Odin", "Phoenix", "Bauhamut", "Knights to the Round", "Leviathan"],
-        answer: "Knights to the Round"
+        choice:["Cloud", "Squall", "Zidane", "Titus", "Cecil"],
+        answer: "Cloud"
     },
     {
         title: "Who is the main antagonist of the game?",
-        choice:["Odin", "Phoenix", "Bauhamut", "Knights to the Round", "Leviathan"],
-        answer: "Knights to the Round"
+        choice:["Ultimecia", "Necron", "Kuja", "Sephiroth", "Sin"],
+        answer: "Sephiroth"
     },
     {
         title: "What is the name of the antagonists mother?",
-        choice:["Odin", "Phoenix", "Bauhamut", "Knights to the Round", "Leviathan"],
-        answer: "Knights to the Round"
+        choice:["Aerith", "Tifa", "Lucrecia", "Jenova", "Yuffie"],
+        answer: "Lucrecia"
     },
     {
         title: "Who was Cloud's best friend who died before the events of the game?",
-        choice:["Odin", "Phoenix", "Bauhamut", "Knights to the Round", "Leviathan"],
-        answer: "Knights to the Round"
+        choice:["Cid", "Vincent", "Angel", "Zach", "Barret"],
+        answer: "Zach"
     },
     {
         title: "What is the name of Vincent's final limit break?",
-        choice:["Odin", "Phoenix", "Bauhamut", "Knights to the Round", "Leviathan"],
-        answer: "Knights to the Round"
+        choice:["Omega", "Catastrophe", "Chaos", "Omni-slash", "Alls-end"],
+        answer: "Chaos"
     },
     {
         title: "Who is the father of the main antagonist?",
-        choice:["Odin", "Phoenix", "Bauhamut", "Knights to the Round", "Leviathan"],
-        answer: "Knights to the Round"
+        choice:["Ruffus", "Hojo", "Cid", "Barret", "Vincent"],
+        answer: "Hojo"
     },
     {
         title: "What is the name of the largest city in the game?",
-        choice:["Odin", "Phoenix", "Bauhamut", "Knights to the Round", "Leviathan"],
-        answer: "Knights to the Round"
+        choice:["Midgar", "Kalm", "Edge", "Costa Del Sol", "Mideel"],
+        answer: "Midgar"
     },
     {
         title: "What boss must be fought in order to gain Cloud's ultimate weapon?",
-        choice:["Odin", "Phoenix", "Bauhamut", "Knights to the Round", "Leviathan"],
-        answer: "Knights to the Round"
+        choice:["Ultima Weapon", "Emerald Weapon", "Omega Weapon", "Ruby Weapon", "Diamond Weapon"],
+        answer: "Ultima Weapon"
     },
     {
         title: "What is the name of Bauhamut-ZER0's summon attack?",
-        choice:["Odin", "Phoenix", "Bauhamut", "Knights to the Round", "Leviathan"],
-        answer: "Knights to the Round"
+        choice:["Mega-Flare", "Terra-Flare", "Giga-Flare", "Zetta-Flare", "Shadow-Flare"],
+        answer: "Terra-Flare"
     }
 ];
