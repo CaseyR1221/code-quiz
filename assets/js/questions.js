c2e9062fc5edebe60e8b4e57ad389d4a8ab642c2
Casey R <crowlandsdev@gmail.com>
2021-06-29 23:28:04 -0400

    added js files and questions for the quiz

---
+++ assets/js/questions.js
@@ -0,0 +1,52 @@
+let questions = [
+    {
+        title: "What is the name of the ultimate summon you can obtain in the game?",
+        choice:["Odin", "Phoenix", "Bauhamut", "Knights to the Round", "Leviathan"],
+        answer: "Knights to the Round"
+    },
+    {
+        title: "What is the name of the main character in the game?",
+        choice:["Odin", "Phoenix", "Bauhamut", "Knights to the Round", "Leviathan"],
+        answer: "Knights to the Round"
+    },
+    {
+        title: "Who is the main antagonist of the game?",
+        choice:["Odin", "Phoenix", "Bauhamut", "Knights to the Round", "Leviathan"],
+        answer: "Knights to the Round"
+    },
+    {
+        title: "What is the name of the antagonists mother?",
+        choice:["Odin", "Phoenix", "Bauhamut", "Knights to the Round", "Leviathan"],
+        answer: "Knights to the Round"
+    },
+    {
+        title: "Who was Cloud's best friend who died before the events of the game?",
+        choice:["Odin", "Phoenix", "Bauhamut", "Knights to the Round", "Leviathan"],
+        answer: "Knights to the Round"
+    },
+    {
+        title: "What is the name of Vincent's final limit break?",
+        choice:["Odin", "Phoenix", "Bauhamut", "Knights to the Round", "Leviathan"],
+        answer: "Knights to the Round"
+    },
+    {
+        title: "Who is the father of the main antagonist?",
+        choice:["Odin", "Phoenix", "Bauhamut", "Knights to the Round", "Leviathan"],
+        answer: "Knights to the Round"
+    },
+    {
+        title: "What is the name of the largest city in the game?",
+        choice:["Odin", "Phoenix", "Bauhamut", "Knights to the Round", "Leviathan"],
+        answer: "Knights to the Round"
+    },
+    {
+        title: "What boss must be fought in order to gain Cloud's ultimate weapon?",
+        choice:["Odin", "Phoenix", "Bauhamut", "Knights to the Round", "Leviathan"],
+        answer: "Knights to the Round"
+    },
+    {
+        title: "What is the name of Bauhamut-ZER0's summon attack?",
+        choice:["Odin", "Phoenix", "Bauhamut", "Knights to the Round", "Leviathan"],
+        answer: "Knights to the Round"
+    }
+];
